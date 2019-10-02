d09a7dd033a3057c56df97504fc7ec110b84aa0b
Mario Moreno <mario.moreno.a93@gmail.com>
2019-10-02 08:40:16 -0500

    Marge remote tracking

--- code/Java/helloWorld.java
+++
@@ -1,9 +0,0 @@
-public class HelloWorld {
-	public static void main (String[] args) {
-    try {
-		  System.out.println("Hello, World!");
-    } catch (Exception e) {
-      System.out.println("Hello, World failed!");
-    }
-	}
-}
