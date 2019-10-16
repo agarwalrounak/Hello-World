2ecc164df6cfeaebbc89c77f5dda94954597aa45
anubhavbagri <56643117+anubhavbagri@users.noreply.github.com>
2019-10-17 00:45:18 +0530

    Updated NamasteDuniya.java

--- code/Java/NamasteDuniya.java
+++ code/Java/NamasteDuniya.java
@@ -1,9 +1,14 @@
+import java.util.*;
 public class NamasteDuniya {
   
         public static void main(String[] args) {
-
-                System.out.println("Hello World!");
-
+                Scanner sc=new Scanner(System.in);
+                System.out.println("Enter the number of time you want to print : ");
+                int n =sc.nextInt();
+                for(int i=1;i<=n;i++)
+                {
+                System.out.println("Hello World "+i+" !");
+                }
         }
 
 
