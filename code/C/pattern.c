02286c15a6887b75ed40d36888b3bc721c351e67
anubhavbagri <56643117+anubhavbagri@users.noreply.github.com>
2019-10-17 00:39:31 +0530

    Create pattern.c

---
+++ code/C/pattern.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+  int n,i,j;
+  printf("Enter the number of rows : ");
+  scanf("%d",&n);
+  for(i=1;i<=n;i++)
+  {
+    for(j=1;j<=i;j++)
+    {
+      printf("*");
+    }
+    printf("\n");
+  }
+}
