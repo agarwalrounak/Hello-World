00448a519bef51f004aae76048b8244122b7bb9a
Venkatesh Prasad <vensadk@gmail.com>
2019-10-20 01:19:09 +0530

    Added a new folder for Julia coding. Now one can say hello to the world even in Julia

---
+++ code/hello_world.jl
@@ -0,0 +1,3 @@
+println("Hello World. This is JuliaLang, a language designed for high performance.")
+
+println("It has the speed of C/C++ and the ease of Python. It is revolutionizing scientific coding.")
