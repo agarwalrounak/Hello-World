036883aff09c675aa30c8378bc7eaed592ee8baf
wildy26 <anurag182001sharma@gmail.com>
2019-10-24 23:12:09 +0530

    A C# Program which will display your Name.

---
+++ code/C#/Display_Your_Name_Anurag.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Details_Form
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is Your Name?");
+            Console.Write("Enter Your First Name : ");
+            string myFirstName;
+            myFirstName = Console.ReadLine();
+
+            Console.Write("Enter Your Last Name : ");
+            string myLastName;
+            myLastName = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + myFirstName + " " + myLastName);
+
+            Console.WriteLine("What is Your age?");
+            Console.Write("Enter your Age : ");
+            string myAge;
+            myAge = Console.ReadLine();
+
+            Console.WriteLine("Hi, " + myFirstName + " " + myLastName + " You are " + myAge + " Years Old.");
+            Console.ReadLine();
+        }
+    }
+}
