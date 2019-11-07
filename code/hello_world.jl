50924c5057bc4edd51928602827f3197f0ccbc9b
Rounak Agarwal <rounakr.ag73@gmail.com>
2019-11-07 12:24:50 +0530

    Rename code/hello_world.jl to code/Julia/hello_world.jl

--- code/hello_world.jl
+++
@@ -1,3 +0,0 @@
-println("Hello World. This is JuliaLang, a language designed for high performance.")
-
-println("It has the speed of C/C++ and the ease of Python. It is revolutionizing scientific coding.")
